c39509acfe28935a46827d710a909e4d7bc8c756
Rosie <rleone@sandia.gov>
2020-02-10 14:42:44 -0700

    updated steps for html file

--- src/python/keyword_database/createdatabase.py
+++ src/python/keyword_database/createdatabase.py
@@ -14,6 +14,19 @@ Created on Tue Jan 28 10:40:03 2020
     #a. cd software/pflotran/src/python/keyword_database
     #b. mkdir docs
 #3. Run script
+    
+    
+####STEPS TO CREATE HTML FILE######
+#1. Go into docs directory
+    #a. cd docs
+#2. Setup sphinx (will give error that there is already an index file but ignore this)
+    #a. sphinx-quickstart
+#3. Make the html files
+    #a. make clean
+    #b. make html
+#4. Open up html files
+    #a. cd _build/html
+    #b. open index.html 
 
 import os
 import numpy as np
@@ -95,40 +108,7 @@ for file in files:
 #                if len(words)>1 and words[-1] not in keywords_dict[file]:
 #                    keywords_dict[file].append(words[-1])
 
-###WORKING 
-#with open('{}/keyword_database/index.rst'.format(root_dir),'w') as f:
-#
-#    intro = """
-#*************************
-#PFLOTRAN Keyword Database
-#*************************
-#
-#.. toctree::
-#   :maxdepth: 2
-#""" 
-#        
-#    f.write(intro)
-#
-#    for keyword,filepaths in keywords_dict.items():
-#        keyword_rst = keyword + '.rst'
-#        f.write("""
-#   {}""".format(keyword_rst))
-#    
-#    
-#        with open('{}/keyword_database/{}'.format(root_dir,keyword_rst),'w') as fin:
-#          fin.write('.. _{}:'.format(keyword))
-#          fin.write('\n\n')
-#          fin.write('{}'.format(keyword))
-#          fin.write('\n')
-#          fin.write('{}\n'.format('='*len(keyword)))
-#          fin.write('\n')
-#          for filepath in filepaths:
-#            file = re.split('/|\.',filepath.strip())[-2]
-#            fin.write("""
-#{}
-#            """.format(file))
-            
-##TEST SORT
+
 with open('{}/docs/index.rst'.format(base_dir),'w') as f:
 
     intro = """
@@ -160,96 +140,5 @@ PFLOTRAN Keyword Database
             fin.write("""
 {}
             """.format(file))
-#f = open('{}/keyword_database/index.rst'.format(root_dir),'w')  
-#
-#intro = """
-#*************************
-#PFLOTRAN Keyword Database
-#*************************
-#
-#.. toctree::
-#   :maxdepth: 2
-#""" 
-#        
-#f.write(intro)
-            
-#for keyword,filepaths in keywords_dict.items():
-#    keyword_rst = keyword + '.rst'
-#    f.write("""
-#   {}""".format(keyword_rst))
-#    
-#    
-#    with open('{}/keyword_database/{}'.format(root_dir,keyword_rst),'w') as fin:
-#        fin.write('.. _{}:'.format(keyword))
-#        fin.write('\n\n')
-#        fin.write('{}'.format(keyword))
-#        fin.write('\n')
-#        fin.write('{}\n'.format('='*len(keyword)))
-#        fin.write('\n')
-#        for filepath in filepaths:
-#            file = re.split('/|\.',filepath.strip())[-2]
-#            fin.write("""
-#{}
-#            """.format(file))
-    
-                    
-                    
-                    
-#################################
-#Dictionary based on regress test
-#################################            
-#keywords_dict = {}
-#
-#for file in files:
-#    keywords_dict[file] = []
-#    with open(file,'r') as f:
-#        for line in f:
-#            if line.startswith(' KEYWORD'):
-# #               print(re.split('(:|,|\s)\s*',line))
-#                words = re.split(': |,|\n',line.strip())
-#                if not words[-1].isupper():
-#                    words.pop(-1)
-#                
-#                if len(words)>1 and words[-1] not in keywords_dict[file]:
-#                    keywords_dict[file].append(words[-1])
-                    
-                    
-                
-#                print(line.strip().split(','))#[0].split(':'))
-   
-####Write index file etc     
-#f = open('{}/keyword_database/index.rst'.format(root_dir),'w')  
-#
-#intro = """
-#*************************
-#PFLOTRAN Keyword Database        
-#*************************
-#
-#.. toctree::
-#   :maxdepth: 2
-#""" 
-#        
-#f.write(intro)
-#
-#for filepath,keywords in keywords_dict.items():
-#    file = re.split('/|\.',filepath.strip())[-2] #filename.strip().split('/')[-1]
-#    file_rst = re.split('/|\.',filepath.strip())[-2] + '.rst'
-#    f.write('{}\n'.format(file_rst))
-#    f.write("""
-#   {}""".format(file_rst))
-    
-    
-#    with open('{}/keyword_database/{}'.format(root_dir,file_rst),'w') as fin:
-#        fin.write('.. _{}:'.format(file))
-#        fin.write('\n\n')
-#        fin.write('{}'.format(file))
-#        fin.write('\n')
-#        fin.write('{}\n'.format('='*len(file)))
-#        fin.write('\n')
-#        for keyword in keywords:
-#            fin.write("""
-#{}
-#            """.format(keyword))
-#    print(keywords_dict[file][3])
-    
+
             
