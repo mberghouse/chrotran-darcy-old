775bd295d7faa6a70e26fb00cbde56cd07d7dda0
mberghouse <55556564+mberghouse@users.noreply.github.com>
2023-07-20 22:45:57 -0700

    no message

--- src/pflotran/reaction_sandbox_chrotran.F90
+++ src/pflotran/reaction_sandbox_chrotran.F90
@@ -27,6 +27,7 @@ module Reaction_Sandbox_Chrotran_class
     character(len=MAXWORDLENGTH) :: name_X ! This is for the biocide in the reaction
     character(len=MAXWORDLENGTH) :: name_biomineral ! This is for the dummny bio mineral
 	character(len=MAXWORDLENGTH) :: name_O2 ! This is for the dummny bio mineral
+	character(len=MAXWORDLENGTH) :: name_CO2 ! This is for the dummny bio mineral
 
     PetscInt :: B_id
     PetscInt :: C_id
@@ -36,6 +37,7 @@ module Reaction_Sandbox_Chrotran_class
     PetscInt :: X_id
     PetscInt :: biomineral_id
     PetscInt :: O2_id
+	PetscInt :: CO2_id
 
     ! Decay and inhibition parameters in our sophisticated model
     PetscReal :: background_concentration_B    ! Minimum background concentration of the biomass
@@ -98,6 +100,7 @@ function ChrotranCreate()
   ChrotranCreate%name_I = ''
   ChrotranCreate%name_X = ''
   ChrotranCreate%name_O2 = ''
+  ChrotranCreate%name_CO2 = ''
   ChrotranCreate%name_biomineral = ''
 
   ChrotranCreate%D_mobile_id = 0
@@ -107,6 +110,7 @@ function ChrotranCreate()
   ChrotranCreate%I_id = 0
   ChrotranCreate%X_id = 0
   ChrotranCreate%O2_id = 0
+  ChrotranCreate%CO2_id = 0
   ChrotranCreate%biomineral_id = 0
   
   ChrotranCreate%stoichiometric_D_1 = 0.d0
@@ -235,6 +239,10 @@ subroutine ChrotranRead(this,input,option)
       case('NAME_O2')
         call InputReadWord(input,option,this%name_O2,PETSC_TRUE)
         call InputErrorMsg(input,option,'name_O2', &
+                           'CHEMISTRY,REACTION_SANDBOX,CHROTRAN_PARAMETERS')  
+	  case('NAME_CO2')
+        call InputReadWord(input,option,this%name_CO2,PETSC_TRUE)
+        call InputErrorMsg(input,option,'name_CO2', &
                            'CHEMISTRY,REACTION_SANDBOX,CHROTRAN_PARAMETERS')  
 	  
       case('EXPONENT_B')
@@ -387,7 +395,9 @@ subroutine ChrotranSetup(this,reaction,option)
   this%O2_id = &
     GetPrimarySpeciesIDFromName(this%name_O2, &
                                 reaction,option)
-                                 
+  this%CO2_id = &
+    GetPrimarySpeciesIDFromName(this%name_CO2, &
+                                reaction,option)                               
   this%D_immobile_id = &
     GetImmobileSpeciesIDFromName(this%name_D_immobile, &
                                  reaction%immobile,option)
@@ -430,7 +440,7 @@ subroutine ChrotranReact(this,Residual,Jacobian,compute_derivative, &
   PetscReal :: L_water
   PetscReal :: mu_B, mu_CD
   PetscReal :: sum_food
-  PetscInt :: idof_food_mobile, idof_food_immobile, idof_biomass, idof_Cr, idof_O2
+  PetscInt :: idof_food_mobile, idof_food_immobile, idof_biomass, idof_Cr, idof_O2, idof_CO2
   PetscInt :: idof_alcohol, idof_biomineral, idof_biocide
   PetscReal :: immobile_to_water_vol
   PetscReal :: immobile_mole_fraction, mobile_mole_fraction
@@ -497,6 +507,7 @@ subroutine ChrotranReact(this,Residual,Jacobian,compute_derivative, &
   idof_food_mobile = this%D_mobile_id
   idof_Cr = this%C_id
   idof_O2 = this%O2_id
+  idof_CO2 = this%CO2_id
   idof_alcohol = this%I_id
   idof_biocide = this%X_id
   idof_biomass = reaction%offset_immobile + this%B_id
@@ -532,9 +543,10 @@ subroutine ChrotranReact(this,Residual,Jacobian,compute_derivative, &
   oxygen_rate = -respiration_rate * this%B_id
   
   Residual(idof_O2) = Residual(idof_O2) - oxygen_rate * material_auxvar%volume
+  Residual(idof_CO2) = Residual(idof_CO2) + respiration_rate * material_auxvar%volume
 
   
-Residual(idof_Cr) =      Residual(idof_Cr) + &
+  Residual(idof_Cr) =      Residual(idof_Cr) + &
                            ! Biological reaction, mol/s
                            this%rate_C* &                              ! /s
                            rt_auxvar%immobile(this%B_id)* &                 ! mol/m3 bulk
