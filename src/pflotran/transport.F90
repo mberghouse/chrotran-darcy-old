d97ed36b0c14ce38a9c6b384304f68cfbedf1a0e
mberghouse <55556564+mberghouse@users.noreply.github.com>
2023-11-23 19:54:12 -0800

    no message

--- src/pflotran/transport.F90
+++ src/pflotran/transport.F90
@@ -320,18 +320,19 @@ subroutine TDispersionBC(ibndtype, &
   PetscReal :: hydrodynamic_dispersion(rt_parameter%naqcomp)
   PetscReal :: v_dn
   PetscReal :: vi2_over_v_dn, vj2_over_v_dn, vk2_over_v_dn  
+  logical :: is_biomass
+
+! Additional variables for random factor generation
+  PetscReal :: random_factor
+  PetscInt :: seed, count_rate, count_max
+
+! Generate a random value between 0.5 and 2
+  call random_number(random_factor)
+  random_factor = 0.5 + 1.5 * random_factor 
   PetscReal :: t_ref_inv
 
   nphase = rt_parameter%nphase
-  ! logical :: is_biomass
 
-! ! Additional variables for random factor generation
-  ! PetscReal :: random_factor
-  ! PetscInt :: seed, count_rate, count_max
-
-! ! Generate a random value between 0.5 and 2
-  ! call random_number(random_factor)
-  ! random_factor = 0.5 + 1.5 * random_factor 
   abs_dist_dn(:) = dabs(dist_dn(1:3))
   tran_coefs_over_dist(:,:) = 0.d0    
 
@@ -408,9 +409,9 @@ subroutine TDispersionBC(ibndtype, &
       mechanical_dispersion = dispersivity_dn(LONGITUDINAL)*dabs(q) 
     endif
 	! Modify the dispersion only if the species is biomass
-    ! if (is_biomass) then
-      ! mechanical_dispersion = mechanical_dispersion * random_factor
-    ! endif
+    if (is_biomass) then
+      mechanical_dispersion = mechanical_dispersion * random_factor
+    endif
 	
 
     select case(ibndtype)
